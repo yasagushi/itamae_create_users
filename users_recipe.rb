193ee4998983a42f34daf771d41c8331fb3f2b73
Yasuo Sasaki <y-sasaki@forcus.xyz>
2015-10-09 06:10:59 +0000

    initial commit

---
+++ users_recipe.rb
@@ -0,0 +1,27 @@
+$USERS.each do |u, p|
+  user u do
+    action :create
+    password p
+  end
+
+  SSH_KEY = %x(sudo cat "/home/#{u}/.ssh/id_rsa.pub")
+
+  directory "/home/" + u + "/.ssh" do
+    user u
+    owner u
+    group u
+    mode "700"
+  end
+
+  file "/home/#{u}/.ssh/authorized_keys" do
+    content SSH_KEY
+    owner u
+    group u
+    mode "600"
+  end
+
+  execute "add user to wheel group" do
+    command "usermod -G wheel #{u}"
+    not_if "groups #{u} | grep 'wheel'"
+  end
+end

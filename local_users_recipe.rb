2fdb1b3f23b9cec9759de040bdc46b56bf128283
Yasuo Sasaki <y-sasaki@forcus.xyz>
2015-10-09 09:35:43 +0000

    create user added

--- local_users_recipe.rb
+++ local_users_recipe.rb
@@ -1,6 +1,11 @@
 require "./login_users"
 
 $USERS.each do |u, p|
+  user u do
+    action :create
+    password p
+  end
+
   execute "create user_key" do
     command "sudo su - #{u} -c \"ssh-keygen -t rsa -N #{p} -f ~/.ssh/id_rsa\""
     not_if "test -e /home/#{u}/.ssh/id_rsa.pub"

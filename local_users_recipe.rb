ec2102d2d425d5803be011daa44bbf7f2829ac68
Yasuo Sasaki <y-sasaki@forcus.xyz>
2015-10-12 06:04:18 +0000

    node required

--- local_users_recipe.rb
+++ local_users_recipe.rb
@@ -1,14 +1,15 @@
-require "./login_users"
-
-$USERS.each do |u, p|
+node['users'].length.times do |i|
   salt = ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a).sample(2).join
+  user_name = node['users'][i]['user_name']
+  user_password = node['users'][i]['user_password']
+
   execute "create users" do
-    command "sudo useradd #{u} -p `perl -e \"print(crypt(#{p}, salt));\"`"
-    not_if "grep #{u} /etc/passwd"
+    command "sudo useradd #{user_name} -p `perl -e \"print(crypt(#{user_password}, salt));\"`"
+    not_if "grep #{user_name} /etc/passwd"
   end
 
   execute "create user_key" do
-    command "sudo su - #{u} -c \"ssh-keygen -t rsa -N #{p} -f /home/#{u}/.ssh/id_rsa\""
-    not_if "sudo test -e /home/#{u}/.ssh/id_rsa.pub"
+    command "sudo su - #{user_name} -c \"ssh-keygen -t rsa -N #{user_password} -f /home/#{user_name}/.ssh/id_rsa\""
+    not_if "sudo test -e /home/#{user_name}/.ssh/id_rsa.pub"
   end
 end
--- remote_users_recipe.rb
+++ remote_users_recipe.rb
@@ -1,22 +1,23 @@
-require "./login_users"
-
-$USERS.each do |u, p|
+node['users'].times do |i|
   salt = ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a).sample(2).join
+  user_name = node['users'][i]['user_name']
+  user_password = node['users'][i]['user_password']
+
   execute "create users" do
-    command "sudo useradd #{u} -p `perl -e \"print(crypt(#{p}, salt));\"`"
-    not_if "grep #{u} /etc/passwd"
+    command "sudo useradd #{user_name} -p `perl -e \"print(crypt(#{user_password}, salt));\"`"
+    not_if "grep #{user_name} /etc/passwd"
   end
 
-  SSH_KEY = %x(sudo cat "/home/#{u}/.ssh/id_rsa.pub")
+  SSH_KEY = %x(sudo cat "/home/#{user_name}/.ssh/id_rsa.pub")
 
-  directory "/home/#{u}/.ssh" do
+  directory "/home/#{user_name}/.ssh" do
     user u
     owner u
     group u
     mode "700"
   end
 
-  file "/home/#{u}/.ssh/authorized_keys" do
+  file "/home/#{user_name}/.ssh/authorized_keys" do
     content SSH_KEY
     owner u
     group u
@@ -24,7 +25,7 @@ $USERS.each do |u, p|
   end
 
   execute "add user to wheel group" do
-    command "usermod -G wheel #{u}"
-    not_if "groups #{u} | grep 'wheel'"
+    command "usermod -G wheel #{user_name}"
+    not_if "groups #{user_name} | grep 'wheel'"
   end
 end

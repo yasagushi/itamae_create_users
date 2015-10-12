9ffd645a30d33e1f35df5b18721591f36f8d34ab
Yasuo Sasaki <y-sasaki@forcus.xyz>
2015-10-12 00:30:53 +0000

    useradd change with password

--- local_users_recipe.rb
+++ local_users_recipe.rb
@@ -1,8 +1,9 @@
 require "./login_users"
 
 $USERS.each do |u, p|
+  salt = ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a).sample(2).join
   execute "create users" do
-    command "sudo useradd #{u}"
+    command "sudo useradd #{u} -p `perl -e \"print(crypt(#{p}, salt));\"`"
     not_if "grep #{u} /etc/passwd"
   end
 
